49fc1d46703a48b373f3e27363ddf4d598a38bff
Etka Kocak <ek223zf@student.lnu.se>
2025-02-22 20:55:21 +0100

    linters fix

--- src/model/snippet.js
+++ src/model/snippet.js
@@ -1,11 +1,11 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 
 const SnippetSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    code: { type: String, required: true },
-    language: { type: String, required: true },
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    createdAt: { type: Date, default: Date.now }
-});
+  title: { type: String, required: true },
+  code: { type: String, required: true },
+  language: { type: String, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  createdAt: { type: Date, default: Date.now }
+})
 
-export default mongoose.model('Snippet', SnippetSchema);
+export default mongoose.model('Snippet', SnippetSchema)
