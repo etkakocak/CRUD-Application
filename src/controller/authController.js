cdd3f90789527655331f36664b51a307219dfe67
Etka Kocak <ek223zf@student.lnu.se>
2025-02-22 21:09:01 +0100

    linters fix

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -1,54 +1,54 @@
-import User from '../model/user.js';
+import User from '../model/user.js'
 
 export const registerUser = async (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body
 
-    try {
-        const userExists = await User.findOne({ username });
+  try {
+    const userExists = await User.findOne({ username })
 
-        if (userExists) {
-            req.flash('error_msg', 'Username already exists.');
-            return res.redirect('/auth/register');
-        }
-
-        const user = await User.create({ username, password });
-
-        if (user) {
-            req.session.user = { id: user._id.toString(), username: user.username };
-            req.flash('success_msg', 'Registration successful! You are now logged in.');
-            res.redirect('/snippets');
-        } else {
-            req.flash('error_msg', 'Invalid user data');
-            res.redirect('/auth/register');
-        }
-    } catch (error) {
-        req.flash('error_msg', 'Server error');
-        res.redirect('/auth/register');
+    if (userExists) {
+      req.flash('error_msg', 'Username already exists.')
+      return res.redirect('/auth/register')
     }
-};
+
+    const user = await User.create({ username, password })
+
+    if (user) {
+      req.session.user = { id: user._id.toString(), username: user.username }
+      req.flash('success_msg', 'Registration successful! You are now logged in.')
+      res.redirect('/snippets')
+    } else {
+      req.flash('error_msg', 'Invalid user data')
+      res.redirect('/auth/register')
+    }
+  } catch (error) {
+    req.flash('error_msg', 'Server error')
+    res.redirect('/auth/register')
+  }
+}
 
 export const loginUser = async (req, res) => {
-    const { username, password } = req.body;
+  const { username, password } = req.body
 
-    try {
-        const user = await User.findOne({ username });
+  try {
+    const user = await User.findOne({ username })
 
-        if (!user || !(await user.matchPassword(password))) {
-            req.flash('error_msg', 'Invalid username or password');
-            return res.redirect('/auth/login');
-        }
-
-        req.session.user = { id: user._id.toString(), username: user.username };
-        req.flash('success_msg', `Login successful! Welcome, ${user.username}.`);
-        res.redirect('/snippets');
-    } catch (error) {
-        req.flash('error_msg', 'Server error');
-        res.redirect('/auth/login');
+    if (!user || !(await user.matchPassword(password))) {
+      req.flash('error_msg', 'Invalid username or password')
+      return res.redirect('/auth/login')
     }
-};
+
+    req.session.user = { id: user._id.toString(), username: user.username }
+    req.flash('success_msg', `Login successful! Welcome, ${user.username}.`)
+    res.redirect('/snippets')
+  } catch (error) {
+    req.flash('error_msg', 'Server error')
+    res.redirect('/auth/login')
+  }
+}
 
 export const logoutUser = (req, res) => {
-    req.session.destroy(() => {
-        res.redirect('/');
-    });
-};
+  req.session.destroy(() => {
+    res.redirect('/')
+  })
+}
--- src/controller/snippetController.js
+++ src/controller/snippetController.js
@@ -1,128 +1,128 @@
-import Snippet from '../model/snippet.js';
+import Snippet from '../model/snippet.js'
 
 export const createSnippet = async (req, res) => {
-    const { title, code, language } = req.body;
+  const { title, code, language } = req.body
 
-    if (!req.session.user) {
-        req.flash('error_msg', 'Unauthorized! Please log in.');
-        return res.redirect('/auth/login');
-    }
+  if (!req.session.user) {
+    req.flash('error_msg', 'Unauthorized! Please log in.')
+    return res.redirect('/auth/login')
+  }
 
-    try {
-        const newSnippet = await Snippet.create({
-            title,
-            code,
-            language,
-            user: req.session.user.id
-        });
+  try {
+    await Snippet.create({
+      title,
+      code,
+      language,
+      user: req.session.user.id
+    })
 
-        req.flash('success_msg', 'Snippet created successfully!');
-        res.redirect('/snippets');
-    } catch (error) {
-        req.flash('error_msg', 'Server error');
-        res.redirect('/snippets/create');
-    }
-};
+    req.flash('success_msg', 'Snippet created successfully!')
+    res.redirect('/snippets')
+  } catch (error) {
+    req.flash('error_msg', 'Server error')
+    res.redirect('/snippets/create')
+  }
+}
 
 export const getAllSnippets = async (req, res) => {
-    try {
-        const snippets = await Snippet.find().populate('user', 'username');
-        res.json(snippets);
-    } catch (error) {
-        res.status(500).json({ message: "Server error", error: error.message });
-    }
-};
+  try {
+    const snippets = await Snippet.find().populate('user', 'username')
+    res.json(snippets)
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message })
+  }
+}
 
 export const getUserSnippets = async (req, res) => {
-    if (!req.session.user) {
-        req.flash('error_msg', 'Unauthorized! Please log in.');
-        return res.redirect('/auth/login');
-    }
+  if (!req.session.user) {
+    req.flash('error_msg', 'Unauthorized! Please log in.')
+    return res.redirect('/auth/login')
+  }
 
-    try {
-        const snippets = await Snippet.find({ user: req.session.user.id });
-        res.json(snippets);
-    } catch (error) {
-        res.status(500).json({ message: "Server error", error: error.message });
-    }
-};
+  try {
+    const snippets = await Snippet.find({ user: req.session.user.id })
+    res.json(snippets)
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error: error.message })
+  }
+}
 
 export const getSnippetById = async (req, res) => {
-    try {
-        const snippet = await Snippet.findById(req.params.id);
-        if (!snippet) {
-            return res.status(404).send("Snippet not found");
-        }
-
-        if (snippet.user.toString() !== req.session.user.id) {
-            return res.status(403).send("You are not authorized to edit this snippet");
-        }
-
-        res.render('edit-snippet', { snippet });
-    } catch (error) {
-        res.status(500).send("Server error");
+  try {
+    const snippet = await Snippet.findById(req.params.id)
+    if (!snippet) {
+      return res.status(404).send('Snippet not found')
     }
-};
+
+    if (snippet.user.toString() !== req.session.user.id) {
+      return res.status(403).send('You are not authorized to edit this snippet')
+    }
+
+    res.render('edit-snippet', { snippet })
+  } catch (error) {
+    res.status(500).send('Server error')
+  }
+}
 
 export const updateSnippet = async (req, res) => {
-    const { id } = req.params;
-    const { title, code, language } = req.body;
+  const { id } = req.params
+  const { title, code, language } = req.body
 
-    if (!req.session.user) {
-        req.flash('error_msg', 'Unauthorized! Please log in.');
-        return res.redirect('/auth/login');
+  if (!req.session.user) {
+    req.flash('error_msg', 'Unauthorized! Please log in.')
+    return res.redirect('/auth/login')
+  }
+
+  try {
+    const snippet = await Snippet.findById(id)
+    if (!snippet) {
+      req.flash('error_msg', 'Snippet not found')
+      return res.redirect('/snippets')
     }
 
-    try {
-        const snippet = await Snippet.findById(id);
-        if (!snippet) {
-            req.flash('error_msg', 'Snippet not found');
-            return res.redirect('/snippets');
-        }
-
-        if (snippet.user.toString() !== req.session.user.id) {
-            req.flash('error_msg', 'You can only update your own snippets');
-            return res.redirect('/snippets');
-        }
-
-        snippet.title = title || snippet.title;
-        snippet.code = code || snippet.code;
-        snippet.language = language || snippet.language;
-        await snippet.save();
-
-        req.flash('success_msg', 'Snippet updated successfully!');
-        res.redirect('/snippets');
-    } catch (error) {
-        req.flash('error_msg', 'Server error');
-        res.redirect('/snippets');
+    if (snippet.user.toString() !== req.session.user.id) {
+      req.flash('error_msg', 'You can only update your own snippets')
+      return res.redirect('/snippets')
     }
-};
+
+    snippet.title = title || snippet.title
+    snippet.code = code || snippet.code
+    snippet.language = language || snippet.language
+    await snippet.save()
+
+    req.flash('success_msg', 'Snippet updated successfully!')
+    res.redirect('/snippets')
+  } catch (error) {
+    req.flash('error_msg', 'Server error')
+    res.redirect('/snippets')
+  }
+}
 
 export const deleteSnippet = async (req, res) => {
-    const { id } = req.params;
+  const { id } = req.params
 
-    if (!req.session.user) {
-        req.flash('error_msg', 'Unauthorized! Please log in.');
-        return res.redirect('/auth/login');
+  if (!req.session.user) {
+    req.flash('error_msg', 'Unauthorized! Please log in.')
+    return res.redirect('/auth/login')
+  }
+
+  try {
+    const snippet = await Snippet.findById(id)
+    if (!snippet) {
+      req.flash('error_msg', 'Snippet not found')
+      return res.redirect('/snippets')
     }
 
-    try {
-        const snippet = await Snippet.findById(id);
-        if (!snippet) {
-            req.flash('error_msg', 'Snippet not found');
-            return res.redirect('/snippets');
-        }
-
-        if (snippet.user.toString() !== req.session.user.id) {
-            req.flash('error_msg', 'You can only delete your own snippets');
-            return res.redirect('/snippets');
-        }
-
-        await Snippet.findByIdAndDelete(id);
-        req.flash('success_msg', 'Snippet deleted successfully!');
-        res.redirect('/snippets');
-    } catch (error) {
-        req.flash('error_msg', 'Server error');
-        res.redirect('/snippets');
+    if (snippet.user.toString() !== req.session.user.id) {
+      req.flash('error_msg', 'You can only delete your own snippets')
+      return res.redirect('/snippets')
     }
-};
+
+    await Snippet.findByIdAndDelete(id)
+    req.flash('success_msg', 'Snippet deleted successfully!')
+    res.redirect('/snippets')
+  } catch (error) {
+    req.flash('error_msg', 'Server error')
+    res.redirect('/snippets')
+  }
+}
